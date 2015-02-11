9b9768f07b3bbe6115ebec0dd2e6793982ceaf0a
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-02-11 17:14:29 +0100

    init

---
+++ strcmp_test.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+
+int	strcmp(char *s1, char *s2);
+
+int	test_strcmp()
+{
+  assert(strcmp("abc", "ab") == 1);
+  assert(strcmp("ab", "abc") == -1);
+  assert(strcmp("abc", "abc") == 0);
+}
+
+int main()
+{
+  test_strcmp();
+  return (0);
+}
